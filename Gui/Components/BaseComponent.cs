15d10a49f5028aea1e4fd7c3cd1f4da486f185cc
MrYoranimo <yvulker@gmail.com>
2022-02-18 02:10:09 +0100

    Make declarations for Padding* in IComponent

--- Gui/Components/BaseComponent.cs
+++ Gui/Components/BaseComponent.cs
@@ -8,50 +8,40 @@ public abstract class BaseComponent : IComponent {
 
 	public string Tag { get; set; } = "";
 	public string Text { get; set; } = "";
-
+	public Point Position { get; set; } = new Point(0, 0);
+	public Size DisplayedSize { get; set; } = new Size(0, 0);
 	public IParent? Parent { get; set; } = null;
-
-	public Point Position { get; set; }
+	public Color? BackgroundColor { get; set; }
 	public Margin MarginalSpace { get; set; } = new Margin(0);
 	public Margin BorderSpace { get; set; } = new Margin(0);
 
 	public int PaddingLeft {
 		get => MarginalSpace.Left + BorderSpace.Left;
-		private set { }
 	}
 
 	public int PaddingTop {
 		get => MarginalSpace.Top + BorderSpace.Top;
-		private set { }
 	}
 
-	public int PaddingRight {
+	public int PaddingRight	{
 		get => MarginalSpace.Right + BorderSpace.Right;
-		private set { }
 	}
 
 	public int PaddingBottom {
 		get => MarginalSpace.Bottom + BorderSpace.Bottom;
-		private set { }
 	}
 
 	public int PaddingHorizontal {
 		get => PaddingLeft + PaddingRight;
-		private set { }
 	}
 
 	public int PaddingVertical {
 		get => PaddingTop + PaddingBottom;
-		private set { }
 	}
 
 	public Rectangle Rectangle {
 		get => new(Position.X + PaddingLeft, Position.Y + PaddingTop, DisplayedSize.Width, DisplayedSize.Height);
-		private set { }
 	}
-
-	public Size DisplayedSize { get; set; }
-	public Color? BackgroundColor { get; set; }
 
 	public abstract void Draw(SpriteBatch sb);
 	public abstract void Init();
