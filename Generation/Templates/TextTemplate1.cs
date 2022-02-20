f645e541b305ddfd6c991e6b8ff60279134cd7bf
Koko (MN) <mn.noppen@hotmail.com>
2022-02-20 21:41:29 +0100

    Removed; Old gen script. Added; Textfield as param

--- Generation/Templates/TextTemplate1.cs
+++ Generation/Templates/TextTemplate1.cs
@@ -8,22 +8,50 @@
 using Koko.RunTimeGui.Gui.Initable_Components;
 using Koko.RunTimeGui;
 
-namespace Koko.RuntimeGui { 
+namespace Koko.Generated {
 	public class Generate : IInitable {
 
 		private IParent component = GUI.Gui;
 		private BaseComponent temp;
 
 		public void Init() {
-			 component = new Nav() {Parent = component, };  temp = new Button() {Parent = component, MarginalSpace = new Margin(5),BorderSpace = new Margin(1),};  component.AddChild(temp);  temp = new Button() {Parent = component, MarginalSpace = new Margin(5),BorderSpace = new Margin(1),};  component.AddChild(temp); 			((BaseComponent)component).Parent.AddChild((BaseComponent)component);
+			component = new Nav() { Parent = component, };
+			temp = new Button() { Parent = component, MarginalSpace = new Margin(5), BorderSpace = new Margin(1), };
+			temp.Text = "Inventory";
+			component.AddChild(temp);
+			temp = new Button() { Parent = component, MarginalSpace = new Margin(5), BorderSpace = new Margin(1), };
+			temp.Text = "Inventory2";
+			component.AddChild(temp);
+			((BaseComponent)component).Parent.AddChild((BaseComponent)component);
 			component = ((BaseComponent)component).Parent;
-			 component = new GridPanel() {Parent = component, MarginalSpace = new Margin(10),};  component = new Panel() {Parent = component, MarginalSpace = new Margin(5),BorderSpace = new Margin(1),};  temp = new Checkbox() {Parent = component, MarginalSpace = new Margin(5),};  component.AddChild(temp);  temp = new Button() {Parent = component, MarginalSpace = new Margin(5),BorderSpace = new Margin(1),};  component.AddChild(temp); 			((BaseComponent)component).Parent.AddChild((BaseComponent)component);
+			component = new GridPanel() { Parent = component, MarginalSpace = new Margin(10), };
+			component = new Panel() { Parent = component, MarginalSpace = new Margin(5), BorderSpace = new Margin(1), };
+			temp = new Checkbox() { Parent = component, MarginalSpace = new Margin(5), };
+			temp.Text = "Yoran likes boys? 冰淇淋";
+			component.AddChild(temp);
+			temp = new Button() { Parent = component, MarginalSpace = new Margin(5), BorderSpace = new Margin(1), };
+			temp.Text = "Quit";
+			component.AddChild(temp);
+			((BaseComponent)component).Parent.AddChild((BaseComponent)component);
 			component = ((BaseComponent)component).Parent;
-			 component = new GridPanel() {Parent = component, MarginalSpace = new Margin(5),BorderSpace = new Margin(1),};  temp = new Label() {Parent = component, MarginalSpace = new Margin(2),};  component.AddChild(temp);  temp = new Label() {Parent = component, MarginalSpace = new Margin(2),};  component.AddChild(temp);  temp = new Label() {Parent = component, MarginalSpace = new Margin(2),};  component.AddChild(temp);  temp = new Label() {Parent = component, MarginalSpace = new Margin(2),};  component.AddChild(temp); 			((BaseComponent)component).Parent.AddChild((BaseComponent)component);
+			component = new GridPanel() { Parent = component, MarginalSpace = new Margin(5), BorderSpace = new Margin(1), };
+			temp = new Label() { Parent = component, MarginalSpace = new Margin(2), };
+			temp.Text = "Name:";
+			component.AddChild(temp);
+			temp = new Label() { Parent = component, MarginalSpace = new Margin(2), };
+			temp.Text = "Piet";
+			component.AddChild(temp);
+			temp = new Label() { Parent = component, MarginalSpace = new Margin(2), };
+			temp.Text = "Age:";
+			component.AddChild(temp);
+			temp = new Label() { Parent = component, MarginalSpace = new Margin(2), };
+			temp.Text = "18";
+			component.AddChild(temp);
+			((BaseComponent)component).Parent.AddChild((BaseComponent)component);
 			component = ((BaseComponent)component).Parent;
-						((BaseComponent)component).Parent.AddChild((BaseComponent)component);
+			((BaseComponent)component).Parent.AddChild((BaseComponent)component);
 			component = ((BaseComponent)component).Parent;
-					}
+		}
 	}
 }
 
